b232cb45821d7160382864f0de9ee9fa28caeac6
Samuel Lopez <samuel.lopez@boom.camp>
2020-11-17 09:50:21 +0800

    Mern stack initial

---
+++ server/controllers/posts.js
@@ -0,0 +1,23 @@
+import PostMessage from "../models/postsMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+    console.log(postMessages);
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPosts = async (req, res) => {
+  const post = req.body;
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (err) {
+    res.status(409).json({ message: err.message });
+  }
+};
